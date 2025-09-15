ac9e70084d63675a8740f2adb59221bc9088afce
jules.raitiere-delsupexhe <jules.raitiere-delsupexhe@epita.fr>
2025-09-15 14:43:19 +0200

    Level 1: Tracking experiment

--- ModelMLflowTracking.py
+++ ModelMLflowTracking.py
@@ -82,19 +82,3 @@ with mlflow.start_run():
         # (saving) the model.
         registered_model_name="Iris-model",
     )
-
-# Load the model as a Python Function (pyfunc) and use it for inference
-
-# Load the model back for predictions as a generic Python Function model
-"""loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
-
-# Running Predict on new data using the loaded model.
-predictions = loaded_model.predict(X_test)
-
-iris_feature_names = datasets.load_iris().feature_names
-
-result = pd.DataFrame(X_test, columns=iris_feature_names)
-result["actual_class"] = y_test
-result["predicted_class"] = predictions
-
-result[:4]"""
